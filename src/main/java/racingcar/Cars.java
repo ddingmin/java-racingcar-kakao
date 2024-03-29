05e47d9ba721e98f37efe3f6f1e721e4a5fa5ecc
ddingmin <ddingminyt@gmail.com>
2024-03-29 23:09:29 +0900

    feat: 자동차 일급 콜렉션 구현

---
+++ src/main/java/racingcar/Cars.java
@@ -0,0 +1,49 @@
+package racingcar;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Cars {
+    private final List<Car> cars;
+
+    public Cars(List<Car> cars) {
+        validateDuplicate(cars);
+        this.cars = cars;
+    }
+
+    public Cars(String[] cars) {
+        this(Arrays.stream(cars)
+                .map(Car::new)
+                .collect(Collectors.toList()));
+    }
+
+    public Cars(String input) {
+        this(input.split(","));
+    }
+
+    public int size() {
+        return this.cars.size();
+    }
+
+    public List<Car> getFarthest() {
+        int farthestPosition = getFarthestPosition();
+        return this.cars.stream()
+                .filter(it -> farthestPosition == it.getPosition())
+                .collect(Collectors.toList());
+    }
+
+    private int getFarthestPosition() {
+        return this.cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElseThrow();
+    }
+
+    private void validateDuplicate(List<Car> cars) {
+        if (new HashSet<>(cars).size() != cars.size()) {
+            throw new IllegalArgumentException("중복된 자동차가 존재할 수 없습니다.");
+        }
+    }
+}

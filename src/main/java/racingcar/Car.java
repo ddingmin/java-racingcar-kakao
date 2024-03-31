95cbe0a26f050234a8d72b4f0cd1ea1d9a27cb44
ddingmin <ddingminyt@gmail.com>
2024-03-31 16:26:45 +0900

    refactor: 매직 넘버 상수화

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -4,6 +4,8 @@ import java.util.Objects;
 
 public class Car {
     public static final int INIT_POSITION = 1;
+    public static final int MAX_NAME_LENGTH = 5;
+    public static final int MOVE_BOUND = 4;
 
     private final String name;
     private int position;
@@ -22,7 +24,7 @@ public class Car {
         if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("자동차의 이름은 널 또는 빈값일 수 없습니다");
         }
-        if (name.length() > 5) {
+        if (name.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException("자동차의 이름은 5자 이하만 가능합니다");
         }
     }
@@ -36,7 +38,7 @@ public class Car {
     }
 
     public void move(int randomNumber) {
-        if (randomNumber >= 4) {
+        if (randomNumber >= MOVE_BOUND) {
             this.position++;
         }
     }
